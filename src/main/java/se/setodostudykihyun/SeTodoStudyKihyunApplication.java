cfb082acf1a33354ad376f9a24dc5d1940ba0d13
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-01-12 15:00:58 +0900

    firstCommit

---
+++ src/main/java/se/setodostudykihyun/SeTodoStudyKihyunApplication.java
@@ -0,0 +1,13 @@
+package se.setodostudykihyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeTodoStudyKihyunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeTodoStudyKihyunApplication.class, args);
+    }
+
+}
