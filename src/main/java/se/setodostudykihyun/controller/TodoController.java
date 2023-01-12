21685b57b760e120afdbf94a1284445efb2a1494
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-01-12 15:22:22 +0900

    오류 수정

--- src/main/java/se/setodostudykihyun/controller/TodoController.java
+++ src/main/java/se/setodostudykihyun/controller/TodoController.java
@@ -1,8 +1,14 @@
-package se.setodostudykihyun.controller;
+package com.selab.todo.controller;
+
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import se.setodostudykihyun.dto.request.TodoRegisterRequest;
 import se.setodostudykihyun.dto.response.TodoResponse;
 import se.setodostudykihyun.service.TodoService;
@@ -11,16 +17,15 @@ import se.setodostudykihyun.service.TodoService;
 @RequestMapping(value = "/api/v1/todos", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class TodoController {
-
     private final TodoService todoService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String TodoResponse register(@RequestBody TodoRegisterRequest request){
+    public TodoResponse register(@RequestBody TodoRegisterRequest request) {
         return todoService.register(request);
     }
 
     @GetMapping("/{id}")
-    public TodoResponse get(@PathVariable Long id){
+    public TodoResponse get(@PathVariable Long id) {
         return todoService.get(id);
     }
 }
